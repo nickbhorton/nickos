7b15e45a35c245bc73c44237fc67ea0382e21540
Nick Horton <nickkbhorton@gmail.com>
2024-03-21 11:41:37 -0500

    adding unsigned integer support in printf with a decimal uppercase and lowercase hex conversion specifier

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -14,8 +14,10 @@ void kernel_main(void) {
     printf("strcmp(\"%s\", \"%s\") = %d\n", "Geeks", "Geeks", strcmp("Geeks", "Geeks"));
     printf("strcmp(\"%s\", \"%s\") = %d\n", "zfz", "gfg", strcmp("zfz", "gfg"));
     printf("strcmp(\"%s\", \"%s\") = %d\n", "bfb", "gfg", strcmp("bfb", "gfg"));
-    printf("strcmp(\"%s\", \"%s\") = %d\n", "Geekshello", "Geeks", strcmp("Geekshello", "Geeks"));
-    printf("strcmp(\"%s\", \"%s\") = %d\n", "Geeks", "Geekshello", strcmp("Geeks", "Geekshello"));
+    printf("strcmp(\"%s\", \"%s\") = %i\n", "Geekshello", "Geeks", strcmp("Geekshello", "Geeks"));
+    printf("strcmp(\"%s\", \"%s\") = %i\n", "Geeks", "Geekshello", strcmp("Geeks", "Geekshello"));
+    printf("d(-10)%d i(-10)%i s('string')%s u(10)%u u(-10)%u\n", -10, -10, "string", 10, -10);
+    printf("48190412:%u 48190412:%x 48190412:%X", 48190412, 48190412, 48190412);
 }
 
 
--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -27,10 +27,123 @@ static bool printc(char data)
     return true;
 }
 
-static char digit_to_char(int digit) { return (char)((digit + 48) % 128); }
+static char int_to_digit_char(int digit) { return (char)((digit + 48) % 128); }
+static char uint_to_digit_char(unsigned int digit)
+{
+    return (char)((digit + 48) % 128);
+}
+static char uint_to_lc_hexdigit_char(unsigned int digit)
+{
+    switch (digit)
+    {
+        case 1:
+            return '1';
+            break;
+        case 2:
+            return '2';
+            break;
+        case 3:
+            return '3';
+            break;
+        case 4:
+            return '4';
+            break;
+        case 5:
+            return '5';
+            break;
+        case 6:
+            return '6';
+            break;
+        case 7:
+            return '7';
+            break;
+        case 8:
+            return '8';
+            break;
+        case 9:
+            return '9';
+            break;
+        case 10:
+            return 'a';
+            break;
+        case 11:
+            return 'b';
+            break;
+        case 12:
+            return 'c';
+            break;
+        case 13:
+            return 'd';
+            break;
+        case 14:
+            return 'e';
+            break;
+        case 15:
+            return 'f';
+            break;
+        default:
+            return '0';
+    }
+    return '0';
+}
+
+static char uint_to_uc_hexdigit_char(unsigned int digit)
+{
+    switch (digit)
+    {
+        case 1:
+            return '1';
+            break;
+        case 2:
+            return '2';
+            break;
+        case 3:
+            return '3';
+            break;
+        case 4:
+            return '4';
+            break;
+        case 5:
+            return '5';
+            break;
+        case 6:
+            return '6';
+            break;
+        case 7:
+            return '7';
+            break;
+        case 8:
+            return '8';
+            break;
+        case 9:
+            return '9';
+            break;
+        case 10:
+            return 'A';
+            break;
+        case 11:
+            return 'B';
+            break;
+        case 12:
+            return 'C';
+            break;
+        case 13:
+            return 'D';
+            break;
+        case 14:
+            return 'E';
+            break;
+        case 15:
+            return 'F';
+            break;
+        default:
+            return '0';
+    }
+    return '0';
+}
 
 /*
- * limitations: 
+ * limitations:
  *      flags not implemented
  *      optional field width not implemented
  *      optional precision not implemented
@@ -39,7 +152,7 @@ static char digit_to_char(int digit) { return (char)((digit + 48) % 128); }
  *          c -> char
  *          d -> int 4 bytes hardcoded
  *          s -> const char *
-*/
+ */
 int printf(const char *restrict format, ...)
 {
     va_list parameters;
@@ -77,108 +190,211 @@ int printf(const char *restrict format, ...)
 
         const char *format_begun_at = format++;
 
-        if (*format == 'c')
+        switch (*format)
         {
-            format++;
-            char c = (char)va_arg(parameters, int /* char promotes to int */);
-            if (!maxrem)
+            case 'c':
             {
-                // TODO: Set errno to EOVERFLOW.
-                return -1;
+                format++;
+                char c =
+                    (char)va_arg(parameters, int /* char promotes to int */);
+                if (!maxrem)
+                {
+                    // TODO: Set errno to EOVERFLOW.
+                    return -1;
+                }
+                if (!printc(c))
+                {
+                    return -1;
+                }
+                written++;
+                break;
             }
-            if (!printc(c))
+            case 's':
             {
-                return -1;
+                format++;
+                const char *str = va_arg(parameters, const char *);
+                size_t len = strlen(str);
+                if (maxrem < len)
+                {
+                    // TODO: Set errno to EOVERFLOW.
+                    return -1;
+                }
+                if (!print(str, len))
+                {
+                    return -1;
+                }
+                written += len;
+                break;
             }
-            written++;
-        }
-        else if (*format == 's')
-        {
-            format++;
-            const char *str = va_arg(parameters, const char *);
-            size_t len = strlen(str);
-            if (maxrem < len)
+            case 'i':
+            case 'd':
             {
-                // TODO: Set errno to EOVERFLOW.
-                return -1;
-            }
-            if (!print(str, len))
-            {
-                return -1;
-            }
-            written += len;
-        }
-        else if (*format == 'd')
-        {
-            // 2147483647 is 10
-            static const size_t max_int_digits = 10;
-            char digit_string[max_int_digits];
-            memset(digit_string, 0, max_int_digits);
-            // index starting at last digit
-            int digit_string_index = 9;
+                // 2147483647 is 10
+                static const size_t max_digits = 10;
+                char digit_string[max_digits];
+                memset(digit_string, 0, max_digits);
+                // index starting at last digit
+                int digit_string_index = max_digits - 1;
 
-            format++;
-            int number = va_arg(parameters, int);
-            if (!maxrem)
-            {
-                // TODO: Set errno to EOVERFLOW.
-                return -1;
-            }
-            // lol
-            if (number == -2147483648)
-            {
-                if (!print("-2147483648", 11))
+                format++;
+                int number = va_arg(parameters, int);
+                if (!maxrem)
                 {
+                    // TODO: Set errno to EOVERFLOW.
                     return -1;
                 }
-            }
-            else if (number == 0) {
-                if (!print("0", 1))
+                // lol
+                if (number == -2147483648)
                 {
-                    return -1;
-                }
-            }
-            else
-            {
-                if (number < 0)
-                {
-                    // TODO: test this
-                    number = -number;
-                    if (!printc('-'))
+                    if (!print("-2147483648", 11))
                     {
                         return -1;
                     }
-                    written++;
                 }
-                while (number != 0)
+                else if (number == 0)
                 {
-                    digit_string[digit_string_index] =
-                        digit_to_char(number % 10);
-                    digit_string_index--;
-                    number /= 10;
+                    if (!print("0", 1))
+                    {
+                        return -1;
+                    }
                 }
-                const int size_of_digit_string =
-                    max_int_digits - digit_string_index - 1;
-                if (!print(digit_string + digit_string_index + 1,
-                           size_of_digit_string))
+                else
                 {
+                    if (number < 0)
+                    {
+                        // TODO: test this
+                        number = -number;
+                        if (!printc('-'))
+                        {
+                            return -1;
+                        }
+                        written++;
+                    }
+                    while (number != 0)
+                    {
+                        digit_string[digit_string_index] =
+                            int_to_digit_char(number % 10);
+                        digit_string_index--;
+                        number /= 10;
+                    }
+                    const int size_of_digit_string =
+                        max_digits - digit_string_index - 1;
+                    if (!print(digit_string + digit_string_index + 1,
+                               size_of_digit_string))
+                    {
+                        return -1;
+                    }
+                }
+                break;
+            }
+            case 'u':
+            {
+                // 2147483648 is 10
+                static const size_t max_digits = 10;
+                char digit_string[max_digits];
+                memset(digit_string, 0, max_digits);
+                // index starting at last digit
+                int digit_string_index = max_digits - 1;
+
+                format++;
+                unsigned int number = va_arg(parameters, unsigned int);
+                if (!maxrem)
+                {
+                    // TODO: Set errno to EOVERFLOW.
                     return -1;
                 }
+                if (number == 0)
+                {
+                    if (!print("0", 1))
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    while (number != 0)
+                    {
+                        digit_string[digit_string_index] =
+                            uint_to_digit_char(number % 10);
+                        digit_string_index--;
+                        number /= 10;
+                    }
+                    const int size_of_digit_string =
+                        max_digits - digit_string_index - 1;
+                    if (!print(digit_string + digit_string_index + 1,
+                               size_of_digit_string))
+                    {
+                        return -1;
+                    }
+                }
+                break;
             }
-        }
-        else
-        {
-            format = format_begun_at;
-            size_t len = strlen(format);
-            if (maxrem < len)
+            case 'x':
+            case 'X':
             {
-                // TODO: Set errno to EOVERFLOW.
-                return -1;
+                // 2147483648 is 80000000 is hex so 8 digits
+                static const size_t max_digits = 8;
+                char digit_string[max_digits];
+                memset(digit_string, 0, max_digits);
+                // index starting at last digit
+                int digit_string_index = max_digits - 1;
+
+                format++;
+                unsigned int number = va_arg(parameters, unsigned int);
+                if (!maxrem)
+                {
+                    // TODO: Set errno to EOVERFLOW.
+                    return -1;
+                }
+                if (number == 0)
+                {
+                    if (!print("0", 1))
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    while (number != 0)
+                    {
+                        if (*format == 'x')
+                        {
+                            digit_string[digit_string_index] =
+                                uint_to_lc_hexdigit_char(number % 16);
+                        }
+                        else
+                        {
+                            digit_string[digit_string_index] =
+                                uint_to_uc_hexdigit_char(number % 16);
+                        }
+                        digit_string_index--;
+                        number /= 16;
+                    }
+                    const int size_of_digit_string =
+                        max_digits - digit_string_index - 1;
+                    if (!print(digit_string + digit_string_index + 1,
+                               size_of_digit_string))
+                    {
+                        return -1;
+                    }
+                }
+                break;
             }
-            if (!print(format, len))
-                return -1;
-            written += len;
-            format += len;
+            default:
+            {
+                format = format_begun_at;
+                size_t len = strlen(format);
+                if (maxrem < len)
+                {
+                    // TODO: Set errno to EOVERFLOW.
+                    return -1;
+                }
+                if (!print(format, len))
+                    return -1;
+                written += len;
+                format += len;
+                break;
+            }
         }
     }
 
