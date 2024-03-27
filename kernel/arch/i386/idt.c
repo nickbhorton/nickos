23cb88c1325a155da52b7d07ae91ef60bcafe9d2
Nick Horton <nickkbhorton@gmail.com>
2024-03-26 22:31:37 -0500

    refactor idt files

---
+++ kernel/arch/i386/idt.c
@@ -0,0 +1,33 @@
+#include <kernel/idt.h>
+
+idtr_t read_idtr()
+{
+    idtr_t result = {0, 0};
+
+    asm("sidt %0" : "=g"(result));
+
+    return result;
+}
+
+void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags)
+{
+    idtr_t idtstruct = read_idtr();
+    idt_entry_t *idt = (idt_entry_t *)idtstruct.base;
+    idt_entry_t *descriptor = &idt[vector];
+
+    descriptor->isr_low = (uint32_t)isr & 0xFFFF;
+    descriptor->kernel_cs = 0x08; // this value can be whatever offset your
+                                  // kernel code selector is in your GDT
+    descriptor->attributes = flags;
+    descriptor->isr_high = (uint32_t)isr >> 16;
+    descriptor->reserved = 0;
+}
+
+void initialize_idt()
+{
+    for (uint8_t i = 0; i < 32; i++)
+    {
+        idt_set_descriptor(i, isr_stub_table[i], 0x8E);
+    }
+    asm volatile ("sti");
+}
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -11,10 +11,9 @@
 __attribute__((constructor)) void kernel_init(void)
 {
     terminal_initialize();
-    idt_init();
+    initialize_idt();
     char buf[80];
-    int bytes_written = snprintf(
-        buf, 80, "serial_initialize() return value: %d", serial_initialize());
+    int bytes_written = snprintf(buf, 80, "%d", serial_initialize());
     if (bytes_written > 0)
     {
         terminal_write(buf, bytes_written);
@@ -36,20 +35,18 @@ void kernel_main(void)
     written = pde_summary_string(get_pde(768), str, max_len);
     serial_write(str, written);
     serial_print("\n");
-    for (size_t i = 0; i < 1023; i++)
+    for (size_t i = 0; i < 1023; i += 8)
     {
-        written = pte_summary_string(get_pte(768, i), str, max_len);
-        serial_write(str, written);
-        serial_print(" ");
+        for (size_t j = 0; j < 8; j++)
+        {
+            written = pte_summary_string(get_pte(768, i + j), str, max_len);
+            serial_write(str, written);
+            serial_print(" ");
+        }
+        serial_print("\n");
     }
+    asm volatile("int $14");
     /*
-    uint32_t page_dir_address =
-        physical_to_virtual(get_page_directory_address_p());
-    serial_print("page directory\n");
-    serial_print_memory(page_dir_address, 0x1000, 8);
-    serial_print("\n");
-
-    uint32_t gdt_address = get_page_address_p(get_page_table_entry(768, 1022));
     gdtr_t GDTR = read_gdtr();
     serial_print("GDTR size: %X\nGDTR location: %X\ngdt_address: %X\n",
                  GDTR.limit, GDTR.base, gdt_address);
